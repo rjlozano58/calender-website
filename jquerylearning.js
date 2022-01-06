4e98c980f48be121d944a5185bc9e7da62ffe4f7
rjlozano58 <83673956+rjlozano58@users.noreply.github.com>
2022-01-06 14:18:20 -0600

    Add files via upload

---
+++ jquerylearning.js
@@ -0,0 +1,7 @@
+$(".block").click(function(){
+    $(".block1").hide();
+});
+var monthArray = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"];
+function switchCalenderMonth(){
+    
+}
